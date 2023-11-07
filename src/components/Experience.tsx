e949fc8dabed96a0c1226be77179b51a92c233dd
Israelmatusse <israelmathusse451@gmail.com>
2023-11-08 00:18:59 +0200

    implementacao de seccao de time line no portifolio

---
+++ src/components/Experience.tsx
@@ -0,0 +1,70 @@
+
+'use client';
+
+import { Button, Timeline } from 'flowbite-react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Experience=()=> {
+  return (
+    <Timeline>
+          <Timeline.Item>
+        <Timeline.Point />
+        <Timeline.Content>
+          <Timeline.Time>May 2023</Timeline.Time>
+          <Timeline.Title>NETLINE SERV</Timeline.Title>
+          <Timeline.Body>
+          I currently work as a programmer analyst. implementing everything from business process modeling to solution development. <br />
+          I have experience in developing web services using architectures such as REST and GRAPHQL. <br />
+          and work in a microservices environment
+        </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point />
+        <Timeline.Content>
+          <Timeline.Time>September 2022</Timeline.Time>
+          <Timeline.Title>FASOREL SA</Timeline.Title>
+          <Timeline.Body>
+          I worked as an industrial mechanic. ensuring the operation of industrial equipment in the organization's functional units, such as: <br />
+           food oil refinery, soap plant, bottle, filling plant, oil acid plant and others.
+        </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point />
+        <Timeline.Content>
+          <Timeline.Time>September 2021</Timeline.Time>
+          <Timeline.Title>MERECE INDUSTRIES</Timeline.Title>
+          <Timeline.Body>
+          I worked as a technical production operator at the biscuit plant. ensuring full functioning of the packaging line
+        </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point />
+        <Timeline.Content>
+          <Timeline.Time>February 2019</Timeline.Time>
+          <Timeline.Title>CFM-SUL</Timeline.Title>
+          <Timeline.Body>
+            I was an industrial mechanics intern in the general workshops of CFM Maputo. <br />
+            in the departments of workshop conservation and general production.
+          </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point />
+        <Timeline.Content>
+          <Timeline.Time>January 2021</Timeline.Time>
+          <Timeline.Title>STARTIMES</Timeline.Title>
+          <Timeline.Body>
+        I worked as a sales promoter for TMT kits and 3 months later I became team leader
+          </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      
+    
+    
+    </Timeline>
+  );
+};
+export default Experience;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,12 +2,14 @@
 import NavBar from "../components/NavBar";
 import FooterComponent from "../components/Footer";
 import About from "../components/About";
+import Experience from "../components/Experience";
 const Home=()=>{
     return(
         <div>
             
             <NavBar/>
             <About />
+            <Experience />
             <FooterComponent />
         </div>
            
